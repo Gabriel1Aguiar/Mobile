64548e8653e3c6f4e3798f1eaabeb4ff1528be04
Gabriel Aguiar <gabriel.aguiardn23@gmail.com>
2024-02-27 21:36:46 -0300

    codigos

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Ex4 from './src/components/Ex4';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Ex4/>
     </View>
   );
 }
